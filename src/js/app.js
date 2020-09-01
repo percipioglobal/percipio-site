c5862c394e672d0e3e39ad3b33e538fe669f8494
David McGregor <davidjohnmcgregor@gmail.com>
2020-09-01 11:34:31 +0100

    Add social share logo styling

--- src/js/app.js
+++ src/js/app.js
@@ -5,27 +5,6 @@ import icons from './assets/icons.js';
 
 import { createStore } from './stores/store.js'
 
-// importing and setting up Font Awesome
-import { dom, library } from '@fortawesome/fontawesome-svg-core';
-import { 
-    faCloudDownloadAlt as farCloudDownloadAlt,
-    faExternalLinkAlt as farExternalLinkAlt,
-    faFilePdf as farFilePdf,
-    faFileSpreadsheet as farFileSpreadsheet,
-    faFileWord as farFileWord,
-    faFilePowerpoint as farFilePowerPoint,
-    faFileArchive as farFileArchive, 
-} from '@fortawesome/pro-regular-svg-icons';
-
-// load font-awesome libraries
-library.add(farCloudDownloadAlt, farExternalLinkAlt, farFilePdf, farFileSpreadsheet, farFileWord, farFilePowerPoint, farFileArchive);
-
-// convert i tags to SVG
-dom.watch({
-    autoReplaceSvgRoot: document,
-    observeMutationsRoot: document.body
-});
-
 // App main
 const main = async () => {
     // Async load the vue module
