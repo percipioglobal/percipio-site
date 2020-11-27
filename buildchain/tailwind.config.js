dc9e1db1054d552f18a1ba2950a6d6ec363faf07
Stefanie Gevaert <stefanie@koeketienedesign.be>
2020-11-27 13:13:38 +0100

    added inset + group-hover

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -229,12 +229,34 @@ module.exports = {
             minWidth: {
                 '3/4': '75%'
             },
+
+
+            inset: (theme, { negative }) => ({
+                auto: 'auto',
+                ...theme('spacing'),
+                ...negative(theme('spacing')),
+                '1/2': '50%',
+                '1/3': '33.333333%',
+                '2/3': '66.666667%',
+                '1/4': '25%',
+                '2/4': '50%',
+                '3/4': '75%',
+                full: '100%',
+                '-1/2': '-50%',
+                '-1/3': '-33.333333%',
+                '-2/3': '-66.666667%',
+                '-1/4': '-25%',
+                '-2/4': '-50%',
+                '-3/4': '-75%',
+                '-full': '-100%',
+            }),
         },
     },
     variants: {
-        extend: {
-            scale: ['group-hover'],
-        }
+        backgroundColor: ['responsive', 'hover', 'focus', 'group-hover'],
+        inset: ['responsive'],
+        scale: ['group-hover'],
+        textColor: ['responsive', 'hover', 'focus', 'group-hover'],
     },
     corePlugins: {},
     plugins: [require('@tailwindcss/typography')],
