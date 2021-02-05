b626a4b3e040d77e9c37e7951a12c60bcbb04861
Stefanie Gevaert <>
2021-02-05 12:53:36 +0100

    updated grid with square version + updated whitelist

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -30,6 +30,12 @@ module.exports = {
                 /blue-800$/,
                 /purple-600$/,
                 /purple-800$/,
+                /col-span-1$/,
+                /col-span-2$/,
+                /col-span-3$/,
+                /col-span-4$/,
+                /col-span-5$/,
+                /col-span-6$/,
             ],
         }
     },
