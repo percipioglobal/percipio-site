4e9768aac83e72cacad4c7306df0388ad6a11848
Demis Struiksma <contact@demisstruiksma.com>
2021-05-26 13:49:08 +0200

    Split up numeric logic into separate file and functions

---
+++ src/js/handbook.js
@@ -0,0 +1,69 @@
+export const createHandbookLevels = async() => {
+    
+    // Numeric handbook header logic
+    const activeItem = document.querySelector('.handbook-active');
+
+    if  (activeItem) {
+
+        // Calculated number of level in navigation--handbook.twig
+        const activeItemAttr = activeItem.getAttribute('data-active-index');
+
+        entryTitleLevelCreation(activeItemAttr);
+
+        contentTitleLevelCreation(activeItemAttr);
+    }
+}
+
+export const entryTitleLevelCreation = (activeItemAttr) => {
+
+    const entryTitle =  document.querySelector('.header-section-title');
+
+    // Create span element for calculated level number
+    const span = document.createElement('span');
+    // Change span element value to activeItemAttr value
+    span.textContent = activeItemAttr;
+
+    // Combine entry title with calculated level number
+    entryTitle.prepend(span);
+
+}
+
+const contentTitleLevelCreation = (activeItemAttr) => {
+    const handbookElement = document.querySelector('.handbook').getElementsByTagName("*"); // Select all children elements of handbook
+    const arrIndexes = [0, 0, 0, 0, 0]; // Create array for level indexes below activeItemAttr
+    const lutHeadings = ['H2', 'H3', 'H4', 'H5', 'H6']; // Create lookup table for entry headings, instead of if statement
+    let prevIndex = null; // Create starting value for previous heading
+
+    // Convert HTML collection to array
+    const arr = [].slice.call(handbookElement);
+
+    arr.forEach((element) => {
+
+        // Check if nodename is heading
+        if (lutHeadings.includes(element.nodeName)) {
+            const elementLevel = parseInt(element.nodeName.substring(1)); // Converts string to number e.g: H2 -> 2
+            const arrLevel = elementLevel-2; // Get the right array index level e.g: H2 -> 2 = 0 position in arrIndexes
+            arrIndexes[arrLevel]++; // Get the right array index level value and increase by 1 e.g: [0, 0, 0, 0, 0] -> H2 = [1, 0, 0, 0, 0]
+
+            // Check if previous index is bigger than current index, and see if prevIndex exist
+            if (prevIndex > parseInt(elementLevel) && prevIndex) {
+
+                // Reset array level after active heading index
+                // e.g: arrLevel = 0 (H2), prevIndex = 1 (H3) 
+                // [1, 1, 0, 0, 0] -> [2, 0, 0, 0, 0]
+                for(let i = arrLevel+1; i < arrIndexes.length; i++ ) {
+                    arrIndexes[i] = 0;
+                }
+
+            }
+
+            prevIndex = elementLevel; // Sets current element level as previous for next element iteration
+
+            // activeItemAttr: prepend entry title level
+            // Filter: filter out indexes equal to 0, and convert array to string joint by dot,
+            // Title: add element title after the levels
+            element.innerHTML = activeItemAttr + '.' + arrIndexes.filter(index => index > 0).join('.') + ' ' + element.innerHTML;
+        }
+
+    });
+}
