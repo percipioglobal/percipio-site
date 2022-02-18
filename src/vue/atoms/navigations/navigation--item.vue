16f1e6a2c59b373e556e24eb302104b4816f14ba
Demis Struiksma <contact@demisstruiksma.com>
2022-02-18 13:49:53 +0100

    Add contact CTA button for contact nav item

--- src/vue/atoms/navigations/navigation--item.vue
+++ src/vue/atoms/navigations/navigation--item.vue
@@ -1,18 +1,31 @@
 <template>
-    <a
-        :href="item.target[0].url"
-        class="group block text-right capitalize text-base font-bold"
-        :title="item.target[0].title"
-        role="menuitem"
-        tabindex="0"
-        :aria-current="isCurrent ? 'page' : null"
-    >
-        <div class="py-2 px-4 relative overflow-hidden">
-            <span class="relative z-10 hover:text-blue-dark focus:text-blue-dark transition duration-300 flex justify-end items-center">
+    <div>
+        <button 
+            v-if="item.target[0].typeHandle == 'contactPage'" 
+            class="group relative block text-right capitalize text-base font-bold border-2 border-blue-600 text-blue-600 transition duration-300"
+        >
+            <div class="py-3 px-4 absolute z-10 bg-white-100 w-full h-full group-hover:bg-blue-600 group-hover:opacity-10 group-focus:bg-blue-600 group-focus:opacity-20"></div>
+            
+            <span class="py-3 px-4 relative z-20 flex justify-end items-center">
+                {{ item.target[0].title }}
+            </span>   
+        </button>
+
+        <a
+            v-else
+            :href="item.target[0].url"
+            class="block text-right capitalize text-base font-bold"
+            :title="item.target[0].title"
+            role="menuitem"
+            tabindex="0"
+            :aria-current="isCurrent ? 'page' : null"
+        >
+            <span class="py-3 px-4 relative z-10 hover:text-blue-800 focus:text-blue-800 transition duration-300 flex justify-end items-center">
                 {{ item.target[0].title }}
             </span>
-        </div>
-    </a>
+        </a>
+    </div>
+    
 
 </template>
 
