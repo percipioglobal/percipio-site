df10c14248140d91d0df49198055f516aa08ac8f
Demis Struiksma <contact@demisstruiksma.com>
2022-02-18 15:40:20 +0100

    Adjust active/current nav items styling

--- src/vue/atoms/navigations/navigation--item.vue
+++ src/vue/atoms/navigations/navigation--item.vue
@@ -2,7 +2,7 @@
     <div>
         <button 
             v-if="item.target[0].typeHandle == 'contactPage'" 
-            class="group relative mt-3 lg:mt-0 float-right lg:float-none block text-right capitalize text-base font-bold border-2 border-blue-600 text-blue-600 transition duration-300"
+            class="group relative mt-3 lg:mt-0 float-right lg:float-none block text-right capitalize text-4xl lg:text-base font-bold border-2 border-blue-600 text-blue-600 transition duration-300"
         >
             <div class="py-3 px-4 absolute z-10 bg-white-100 w-full h-full group-hover:bg-blue-600 group-hover:opacity-10 group-focus:bg-blue-600 group-focus:opacity-20"></div>
             
@@ -14,14 +14,20 @@
         <a
             v-else
             :href="item.target[0].url"
-            class="block text-right capitalize text-base font-bold"
+            class="block text-right capitalize text-4xl lg:text-base font-bold"
             :title="item.target[0].title"
             role="menuitem"
             tabindex="0"
             :aria-current="isCurrent ? 'page' : null"
         >
-            <span class="py-3 relative z-10 hover:text-blue-800 focus:text-blue-800 transition duration-300 flex justify-end items-center">
+            <span :class="[
+                    'py-3 relative z-10 hover:text-blue-800 focus:text-blue-800 transition duration-300 flex justify-end items-center',
+                    isCurrent ? 'text-blue-600' : 'text-gray-900'
+                ]"
+            >
                 {{ item.target[0].title }}
+
+                <hr v-if="isCurrent" class="absolute mt-6 h-0.5 w-full bg-blue-600">
             </span>
         </a>
     </div>
@@ -44,15 +50,7 @@
         computed: {
             isCurrent()
             {
-                if(this.item && this.item.includeChildren) {
-
-                    return this.location().includes(this.item.target[0].url);
-
-                }else{
-
-                    return this.location() === this.item.target[0].url;
-
-                }
+                return this.location() === this.item.target[0].url;
             }
         },
 
