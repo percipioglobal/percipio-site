9ec882f83cc7b02dbfc961ec7f5af8612d02e82a
Michael Thomas <michael@dadamotion.be>
2022-03-09 14:25:34 +0100

    build(docker): changed ports to 3500/3501/3502

--- buildchain/webpack-settings/dev-server.settings.js
+++ buildchain/webpack-settings/dev-server.settings.js
@@ -9,6 +9,6 @@ module.exports = {
     host: () => process.env.DEVSERVER_HOST || 'localhost',
     https: () => process.env.DEVSERVER_HTTPS || false,
     poll: () => process.env.DEVSERVER_POLL || false,
-    port: () => process.env.DEVSERVER_PORT || 8081,
-    public: () => process.env.DEVSERVER_PUBLIC || 'http://localhost:8081',
+    port: () => process.env.DEVSERVER_PORT || 3501,
+    public: () => process.env.DEVSERVER_PUBLIC || 'http://localhost:3501',
 };
