4aea1156273ccfc70f76881723c30ce11dd60c29
Stefanie Gevaert <>
2022-06-03 17:01:53 +0200

    feat: provided code blocks

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,7 +1,8 @@
 // import App from '@/vue/App.vue'
 // import { createApp } from 'vue'
 import './assets/icons'
-import { init as initNavigation } from './animations/navigation';
+import { init as initNavigation } from './animations/navigation'
+import { init as initCopy } from './utils/copy-to-clipboard'
 
 // Import our CSS
 import '@/css/app.pcss';
@@ -10,6 +11,7 @@ import '@/css/app.pcss';
 const main = async () => {
 
     initNavigation()
+    initCopy()
 
     // const app = createApp(App)
 
---
+++ src/js/utils/copy-to-clipboard.ts
@@ -0,0 +1,19 @@
+export const init = () => {
+    const clipboards = document.querySelectorAll('.copy-to-clipboard')
+
+    clipboards.forEach(clipboard => {
+        const btn = clipboard.querySelectorAll('.btn-copy-to-clipboard')[0]
+        const code = clipboard.querySelectorAll('.code-copy-to-clipboard')[0]
+        btn.addEventListener('click', (evt) => copy(code))
+    })
+}
+
+const copy = (code) => {
+    const range = document.createRange();
+    range.selectNode(code);
+    window.getSelection().removeAllRanges() // clear current selection
+    window.getSelection().addRange(range) // to select text
+    document.execCommand("copy")
+    window.getSelection().removeAllRanges()// to deselect
+    alert("Copy to Clipboard Successfully")
+}
