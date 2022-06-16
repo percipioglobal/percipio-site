929f0407b9a7a01cc9ec21b1c2a22c5135af1dde
Stefanie Gevaert <>
2022-06-16 18:19:04 +0200

    chore: disabled page animation

--- src/js/app.ts
+++ src/js/app.ts
@@ -2,7 +2,7 @@
 // import { createApp } from 'vue'
 import './assets/icons'
 import { init as initNavigation } from './animations/navigation'
-import { init as initPageAnimation } from './animations/page'
+// import { init as initPageAnimation } from './animations/page'
 import { init as initCopy } from './utils/copy-to-clipboard'
 
 // Import our CSS
@@ -12,7 +12,7 @@ import '@/css/app.pcss';
 const main = async () => {
 
     initNavigation()
-    initPageAnimation()
+    // initPageAnimation()
     initCopy()
 
     // const app = createApp(App)
