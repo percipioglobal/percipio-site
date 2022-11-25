5a011631f818246d5b32b5fba5863e4d60aeb7a9
Stefanie Gevaert <>
2022-11-25 16:57:53 +0100

    build: updated the builchain to handle javascript functions

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,8 +1,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+
+import { init as initNavigation } from '@/js/animations/navigation'
+import { init as initPageAnimation } from '@/js/animations/page'
+import { init as initCopy } from '@/js/utils/copy-to-clipboard'
+
 import AnimationMain from '@/vue/atoms/animations/animation-main.vue'
 
 export default defineComponent({
-    components: { 'animations--animation-main' : AnimationMain}
+    components: { 'animations--animation-main' : AnimationMain},
+    mounted() {
+        initNavigation()
+        initPageAnimation()
+        initCopy()
+    }
 })
 </script>
