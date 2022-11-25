4cfca2d62c12145d7c8cb47a8aa5d821f8978656
Craig Clement <craig@percipio.london>
2022-11-25 13:49:58 +0000

    fix: set animation var to collect first and pass animation url to vue component for lottie player

--- src/vue/atoms/animations/animation-main.vue
+++ src/vue/atoms/animations/animation-main.vue
@@ -9,13 +9,10 @@ interface Props {
 
 const props = defineProps<Props>()
 
-const json = ref(null)
 const lottieId = ref(`lottie-${ Math.random().toString(36).substr(2, 9) }`)
 
 onMounted(async () => {
-    // document.getElementById(`${lottieId.value}`).innerHTML = ""
-    json.value = await (await fetch(props.animation)).json()
-    console.log(json.value)
+    document.getElementById(`${lottieId.value}`).innerHTML = ""
 
     lottie.loadAnimation({
         container: document.getElementById(`${lottieId.value}`), // the dom element that will contain the animation
@@ -30,5 +27,9 @@ onMounted(async () => {
 </script>
 
 <template> 
-    <div :id="lottieId" class="(max-width:768px) 100vw', '(min-width:768px) 55vw animate w-full h-full" data-animation="fadeIn"></div>
+    <div 
+        :id="lottieId" 
+        class="animate w-full h-full" 
+        data-animation="fadeIn"
+    />
 </template>
