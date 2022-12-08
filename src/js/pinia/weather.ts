9c0f4ac787881a3f4af43fb6be872fc37827e9a0
Craig Clement <craig@percipio.london>
2022-12-08 17:32:52 +0000

    chore: eslint fixes

--- src/js/pinia/weather.ts
+++ src/js/pinia/weather.ts
@@ -18,7 +18,7 @@ export const useWeatherStore = defineStore('storeWeather', {
         async fetch(location:string, latitude:number, longitude:number) {
             const d = new Date()
             d.setHours(d.getHours() - 1)
-            if (this.timestamp < d || (this.weather[location] ?? null) === null) {
+            if (this.timestamp < d.valueOf() || (this.weather[location] ?? null) === null) {
                 const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${this.apiKey}`)
                 if(response?.data) {
                     this.timestamp = Date.now()
