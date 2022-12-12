8f79902a2d694c8661bddd1aa9ef2c96b6011a5a
Craig Clement <craig@percipio.london>
2022-12-12 16:23:42 +0000

    fix: footer weather commented out in app.vue

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -8,13 +8,13 @@ import { init as initCopy } from '@/js/utils/copy-to-clipboard'
 
 import AnimationMain from '@/vue/atoms/animations/animation-main.vue'
 import ContentCode from '@/vue/molecules/content/content--code.vue'
-import FooterWeather from '@/vue/molecules/footer/footer--weather.vue'
+// import FooterWeather from '@/vue/molecules/footer/footer--weather.vue'
 
 export default defineComponent({
     components: { 
         'animations--animation-main': AnimationMain,
         'content--code': ContentCode,
-        'footer--weather': FooterWeather,
+        // 'footer--weather': FooterWeather,
     },
     mounted() {
         initNavigation()
