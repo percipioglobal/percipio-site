d91419167fc6e9f5aa1a35a8a135029d7f117574
Craig Clement <craig@percipio.london>
2022-12-20 13:23:41 +0000

    chore: linter errors

--- src/vue/molecules/footer/footer--weather.vue
+++ src/vue/molecules/footer/footer--weather.vue
@@ -44,15 +44,42 @@ onMounted(() => {
             class="w-full flex flex-col items-center relative"
         >
             <span class="hidden absolute lg:-mt-6 ml-32 md:block text-white text-right md:text-base text-2xl font-mono after:content-['°']">{{ Math.floor(weather[location]?.main?.temp) }}</span>
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '01d' || weather[location]?.weather[0]?.icon ==='01n'" :animationWeather="ZeroOneD"/>
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '02d' || weather[location]?.weather[0]?.icon ==='02n'" :animationWeather="ZeroTwoD"  />
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '03d' || weather[location]?.weather[0]?.icon ==='03n'" :animationWeather="ZeroThreeD" />
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '04d' || weather[location]?.weather[0]?.icon ==='04n'" :animationWeather="ZeroFourD" />
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '09d' || weather[location]?.weather[0]?.icon ==='09n'" :animationWeather="ZeroNineD" />
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '10d' || weather[location]?.weather[0]?.icon ==='10n'" :animationWeather="OneZeroD" />
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '11d' || weather[location]?.weather[0]?.icon ==='11n'" :animationWeather="OneOneD" />
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '13d' || weather[location]?.weather[0]?.icon ==='13n'" :animationWeather="OneThreeD" />
-            <AnimationWeather v-if="weather[location]?.weather[0]?.icon === '50d' || weather[location]?.weather[0]?.icon === '50n'" :animationWeather="FiveZeroD" />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '01d' || weather[location]?.weather[0]?.icon ==='01n'" 
+                :animation-weather="ZeroOneD"
+            />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '02d' || weather[location]?.weather[0]?.icon ==='02n'" 
+                :animation-weather="ZeroTwoD"  
+            />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '03d' || weather[location]?.weather[0]?.icon ==='03n'" 
+                :animation-weather="ZeroThreeD" 
+            />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '04d' || weather[location]?.weather[0]?.icon ==='04n'" 
+                :animation-weather="ZeroFourD" 
+            />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '09d' || weather[location]?.weather[0]?.icon ==='09n'" 
+                :animation-weather="ZeroNineD" 
+            />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '10d' || weather[location]?.weather[0]?.icon ==='10n'" 
+                :animation-weather="OneZeroD" 
+            />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '11d' || weather[location]?.weather[0]?.icon ==='11n'" 
+                :animation-weather="OneOneD" 
+            />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '13d' || weather[location]?.weather[0]?.icon ==='13n'" 
+                :animation-weather="OneThreeD" 
+            />
+            <AnimationWeather 
+                v-if="weather[location]?.weather[0]?.icon === '50d' || weather[location]?.weather[0]?.icon === '50n'" 
+                :animation-weather="FiveZeroD" 
+            />
             <div class="flex  justify-center items-center">
                 <span class="inline-block text-white prose text-lg md:text-3xl font-medium tracking-tightest prose-strong:text-pink-500 prose-strong:font-normal"> {{ city ? city : '' }} </span>
                 <span class="md:hidden ml-2 text-white text-sm font-mono after:content-['°']">{{ Math.floor(weather[location]?.main?.temp) }}</span>
